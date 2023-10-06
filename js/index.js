18d016972d22f3a2a84060056d7d97f7aae3da29
bricio-imbiki <bricio.imbiki15@gmail.com>
2023-10-06 08:57:14 +0300

    modified:   index.html modified:   js/index.js modified:   styles/index.css

--- js/index.js
+++ js/index.js
@@ -53,33 +53,3 @@ window.addEventListener("scroll", function() {
         banner.style.transform = "translateX(-100%)";
     }
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var nextButton = document.querySelector('.swiper-button-next');
-    var prevButton = document.querySelector('.swiper-button-prev');
-    var images = document.querySelectorAll('.personnage-img');
-
-    nextButton.addEventListener('click', function() {
-        images.forEach(function(image) {
-            var screenWidth = window.innerWidth;
-            var columnWidth = screenWidth / 2;
-            var imageLeftPosition = image.getBoundingClientRect().left;
-            image.classList.remove('img-active');
-            if (imageLeftPosition < columnWidth) {
-                image.classList.add('img-active');
-            }
-        });
-    });
-
-    prevButton.addEventListener('click', function() {
-        images.forEach(function(image) {
-            var screenWidth = window.innerWidth;
-            var columnWidth = screenWidth / 4;
-            var imageRightPosition = image.getBoundingClientRect().right;
-            image.classList.remove('img-active');
-            if (imageRightPosition < columnWidth) {
-                image.classList.add('img-active');
-            }
-        });
-    });
-});
